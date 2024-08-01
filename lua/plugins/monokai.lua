4d093e7ba4ff7a513d96751ea6b5a1f8d18c56fc
blagoySimandov <baggy.simandoff@gmail.com>
2024-08-01 23:30:12 +0300

    init

---
+++ lua/plugins/monokai.lua
@@ -0,0 +1,12 @@
+return {
+  { "loctvl842/monokai-pro.nvim", opts = {
+    filter = "machine",
+  } },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "monokai-pro",
+    },
+  },
+}
