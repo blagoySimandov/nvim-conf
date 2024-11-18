10e972f73b1e73c37e7bbc607e45de1d3f3fda74
blagoySimandov <baggy.simandoff@gmail.com>
2024-11-18 14:24:06 +0000

    updated cmp

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,7 +1,7 @@
 return {
   { "loctvl842/monokai-pro.nvim", opts = {
     filter = "machine",
-    transparent_background = true,
+    transparent_background = false,
   } },
 
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {
