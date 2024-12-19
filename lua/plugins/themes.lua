f7b458bf2815f040550a2a2c7583a8927db713a7
blagoySimandov <baggy.simandoff@gmail.com>
2024-12-19 11:58:25 +0000

    lazyupdate + changes to telescope

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,7 +1,7 @@
 return {
   { "loctvl842/monokai-pro.nvim", opts = {
     filter = "machine",
-    transparent_background = false,
+    transparent_background = true,
   } },
 
   { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = {
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,6 +1,7 @@
 return {
   "folke/which-key.nvim",
   opts = {
+    preset = "classic",
     spec = {
       { "<leader>o", group = "open" },
       { "<leader>ot", group = "terraform" },
